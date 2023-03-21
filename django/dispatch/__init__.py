874cb0d29de4131d7c36ccedc029bb9daf29cbb1
Goudar <113749395+karthikgoudar@users.noreply.github.com>
2023-03-21 13:19:53 +0530

    Update __init__.py
    Fixed alignment in docstring

--- django/dispatch/__init__.py
+++ django/dispatch/__init__.py
@@ -1,4 +1,5 @@
-"""Multi-consumer multi-producer dispatching mechanism
+"""
+Multi-consumer multi-producer dispatching mechanism
 
 Originally based on pydispatch (BSD) https://pypi.org/project/PyDispatcher/2.0.1/
 See license.txt for original license.
